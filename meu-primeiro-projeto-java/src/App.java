eaa37871dbe77d80617a0dbfe6c68298f6627d00
CBSouzaSilva <cbsouzasilva13@gmail.com>
2024-12-05 21:22:27 -0300

    Sincronizando git no pc de Sete Lagoas

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá, Mundo!");
+        System.out.println("sincronizando o Git/GitHub no PC de Sete Lagoas.");
     }
 }
