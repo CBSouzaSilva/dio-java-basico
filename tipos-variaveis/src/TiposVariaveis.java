c672db4cbf81eca73fafd774a326b8b22c30c6a6
CBSouzaSilva <cbsouzasilva13@gmail.com>
2024-12-10 20:03:15 -0300

    aula 4

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -5,7 +5,25 @@ public class TiposVariaveis {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal; //cuidado nas conversões das classes de numeros
-        
+
+
+        //Quando o valor nunca podera mudar!!!
+        //São usados as constantes nestes casos
+        // utilizando a palavra final e sempre 
+        //caixa alta 
+        // exemplo quando o valor muda
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        //Exemplo quando o número não altera
+        final double VALOR_DE_PI = 3.14;
+
+        //tipos primitivos
+        //classe String que representa texto na aplicação
+        //ex
+
+        String meuNome = "Ivan Marcele";
 
     }
 
