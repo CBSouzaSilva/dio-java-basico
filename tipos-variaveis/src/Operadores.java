49475ce577ee0b61b8f0570ea0f89ed2c4c57e45
CBSouzaSilva <cbsouzasilva13@gmail.com>
2024-12-10 20:56:49 -0300

    operadores aula 3

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -7,19 +7,30 @@ public class Operadores {
 
         String concatenacao = "?";
         concatenacao = 1+1+1+"1";
-        System.out.println(concatenacao);
+        //System.out.println(concatenacao);
 
         concatenacao = 1+"1"+1+1+1;
-        System.out.println(concatenacao);
+        //System.out.println(concatenacao);
 
         concatenacao = 1+"1"+1+"1";
-        System.out.println(concatenacao);
+        //System.out.println(concatenacao);
 
         concatenacao = "1"+1+1+1;
-        System.out.println(concatenacao);
+        //System.out.println(concatenacao);
 
         concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);
+        //System.out.println(concatenacao);
 
+        //Operadores Unários
+        int numero = 5;
+        
+        numero = -numero; // deixa o número negativo
+
+        System.out.println(numero); 
+
+        //numero = - numero;// deixa o numero positivo desta forma ou 
+        numero = numero *-1; // desta forma
+        System.out.println(numero);
     }
+    
 }
