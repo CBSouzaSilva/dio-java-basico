3f8719938e784a6d03786b5db4015f130a66f906
CBSouzaSilva <cbsouzasilva13@gmail.com>
2024-12-10 19:45:49 -0300

    aula3

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,14 @@
 public class TiposVariaveis {
-    
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //cuidado nas conversões das classes de numeros
+        
+
+    }
+
+
+
 }
