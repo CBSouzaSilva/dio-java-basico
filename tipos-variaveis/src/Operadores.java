c3ef44bfe71f18c5d5b47169a7aaeb88a497f7e2
CBSouzaSilva <cbsouzasilva13@gmail.com>
2024-12-11 19:06:50 -0300

    operadores aula 4

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -26,11 +26,32 @@ public class Operadores {
         
         numero = -numero; // deixa o número negativo
 
-        System.out.println(numero); 
+        //System.out.println(numero); 
 
         //numero = - numero;// deixa o numero positivo desta forma ou 
         numero = numero *-1; // desta forma
-        System.out.println(numero);
+        //System.out.println(numero);
+        
+        //INCRIMENTO x repetições
+        
+        int numeroI = 5;
+
+        //numeroI++; // isso é igual numeroI = numeroI + 1;
+        //exitem 2 possibilidade
+        
+        System.out.println(++ numeroI); //Neste caso o numeroI é acrecido de 1 e depois é impresso na tela
+
+        //System.out.println(numeroI ++); // neste caso o numeroI impresso na tela e so depois é acrecido de 1, sendo necessário um segundo print para mostra o valor atual
+
+        //System.out.println(numeroI);
+
+        // todas essas regras são identicas para decrementar 
+
+        boolean variavel =true;
+        //System.out.println(!variavel); // porem so modifica em memoria
+
+        variavel = !variavel; //agora muda o valor da variavel 
+        System.out.println(variavel);
     }
     
 }
