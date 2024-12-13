915ff160941027d6fa0febf82724be339889d2a9
CBSouzaSilva <cbsouzasilva13@gmail.com>
2024-12-13 16:51:40 -0300

    Aprendendo sobre JAVA DOCs

---
+++ Java-Terminal/src/Comentarios.java
@@ -0,0 +1,33 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // COMENTARIO EM 1 ÚNICA LINHA       
+        // Olá, eu sou um comentario em uma única linha
+        
+        // COMENTÁRIO EM VARIAS LINHAS
+        /*Olá,
+         * Eu sou um comentário
+         * que pode ser mais detlhado
+         * quando necessário
+         */
+
+         //COMENTARIO DE DOCUMENTAÇÃO
+         /**
+          * estas duas estrelinhas acima
+          é para identificar que você
+          pretende elaborar um comentário
+          a nível de documentação
+          Que incrível!!!
+          */
+    }
+    //COMENTARIO DE DOCUMENTAÇÃO
+         /**
+          * estas duas estrelinhas acima
+          é para identificar que você
+          pretende elaborar um comentário
+          a nível de documentação
+          Que incrível!!!
+          */
+        public void metodo(){
+            
+        }
+}
