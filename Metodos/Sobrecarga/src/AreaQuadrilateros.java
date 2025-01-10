7f3bc3d337252f3660d7ba3a4330e5f95b19e164
CBSouzaSilva <cbsouzasilva13@gmail.com>
2025-01-09 21:03:09 -0300

    sobrecarga e exercicios

---
+++ Metodos/Sobrecarga/src/AreaQuadrilateros.java
@@ -0,0 +1,7 @@
+/*
+exemplo para a aula de sobrecarga:
+Criar uma aplicação que calcule a área dos 3 quadriláteros notáveis:
+Quadrado, retângulo e trapézio.
+ */
+public class AreaQuadrilateros {
+}
